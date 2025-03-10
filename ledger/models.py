019f765164a6126cc510c3337325d8453101d223
Jameel Angeli Abarico <abarico.angeli.jameel@gmail.com>
2025-03-10 19:45:31 +0800

    modified ledger

--- ledger/models.py
+++ ledger/models.py
@@ -19,7 +19,7 @@ class Recipe(models.Model):
         User,
         on_delete=models.CASCADE,
         null=True,
-        blank=True,
+        blank=True
     )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
--- ledger/urls.py
+++ ledger/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
-    path('recipes/list', RecipeListView.as_view(), name="recipe-list"),
-    path('recipes/<int:pk>', RecipeDetailView.as_view(), name="recipe-detail")
+    path('list/', RecipeListView.as_view(), name="recipe-list"),
+    path('<int:pk>/', RecipeDetailView.as_view(), name="recipe-detail")
 ]
 
 app_name = 'ledger'
