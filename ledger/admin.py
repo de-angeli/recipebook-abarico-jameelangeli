56b0f295ec27fd16c87cf2e04060c3a619949ca3
Jameel Angeli Abarico <abarico.angeli.jameel@gmail.com>
2025-03-24 20:06:42 +0800

    added RecipeImage as an inline to Recipe admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient, RecipeImage
+
+
+class RecipeImageInLine(admin.TabularInline):
+    model = RecipeImage
 
 
 class RecipeIngredientInLine(admin.TabularInline):
     model = RecipeIngredient
 
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
-    inlines = [RecipeIngredientInLine,]
+    inlines = [RecipeIngredientInLine, RecipeImageInLine,]
     search_fields = ("name",)
     list_display = ("name",)
     list_filter = ("name",)
